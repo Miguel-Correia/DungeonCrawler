f29884838e81592722524a32c67f9cf16842261b
unknown <miguelncorreia99@gmail.com>
2020-07-27 17:11:21 +0100

    Started working on UI

---
+++ Assets/Scripts/TrackPlayer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackPlayer : MonoBehaviour
+{
+
+    public Transform character;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(character.position.x, character.position.y, -1.0f);
+    }
+}
